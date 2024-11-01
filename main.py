1cfa6f15af06381c2b5b85fbceef74fe9f0baba9
sashin92 <aopaoi0987@naver.com>
2024-11-01 17:01:37 +0900

    [fix]: CORS 설정
    일단 모두 허용으로 설정했습니다

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from router.video_senti import router as video_router
@@ -10,6 +11,15 @@ from router.text_wise_saying import router as text_wise_saying_router
 from router.text_music import router as text_music_router
 
 app = FastAPI()
+
+# CORS 모두 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # router를 app에 등록
 
